6948ce0b5dc8721e790a169c5c8458ce7da1dbad
kkristof200 <kovacskristof200@gmail.com>
2020-11-26 15:47:06 +0200

    no message

--- src/cachable/cachable.ts
+++ src/cachable/cachable.ts
@@ -1,15 +1,35 @@
-import { save, load, exists } from 'syncpathutils'
+import * as spu from 'syncpathutils'
+import * as apu from 'asyncpathutils'
+import { SuccessErrorCallback } from 'successerror'
+
+export type CachableOptions<T> = {
+    value?: T
+    defaultValue?: T
+    saveIfNotExists?: boolean
+}
 
 export class Cachable<T> {
     readonly path: string
+    async: boolean
     value?: T
 
-    constructor(path: string, value?: T, defaultValue: T = null, saveIfNotExists: boolean = false) {
+    private constructor(path: string, value?: T, defaultValue: T = null, saveIfNotExists = false, async = false) {
         this.path = path
+    }
 
-        if (value) {
-            this.value = value
-        } else if (exists(path)) {
+    static sync<T>(path: string, options?: CachableOptions<T>) {
+        const cachable = new Cachable<T>(path)
+        cachable.setupSync(options)
+
+        return cachable
+    }
+
+    private setupSync<T>(options?: CachableOptions<T>) {
+        options = options ?? defaultOptions()
+
+        if (options.value) {
+            this.value = options.value
+        } else if (exists(this.path)) {
             this.load()
         }
 
@@ -25,8 +45,22 @@ export class Cachable<T> {
     save() {
         save(this.path, this.value)
     }
+    saveSync() {
+        spu.save(this.path, this.value)
+    }
+    saveAsync(...callbacks: SuccessErrorCallback[]) {
+        apu.save(this.path, this.value, ...callbacks)
+    }
 
     load(fallbackValue: T = null, saveIfNotExists: boolean = false) {
         this.value = load(this.path, fallbackValue, saveIfNotExists)
+    }
+}
+
+function defaultOptions<T>(): CachableOptions<T> {
+    return {
+        value: null,
+        defaultValue: null,
+        saveIfNotExists: false
     }
 }
